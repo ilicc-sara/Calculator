766277e79c32b0fa276206f799cc34f8c427f85f
Sara <sarailic431@gmail.com>
2024-10-31 14:17:59 +0100

    adding html and css

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+"use strict";
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let firstNumber;
+let secondNumber;
+let operation;
+let result;
 
-setupCounter(document.querySelector('#counter'))
+const numBtns = document.querySelectorAll(".op-btn");
+
+numBtns.forEach(function (num, i) {
+  num.addEventListener("click", function (e) {
+    console.log(e.target.dataAttribute);
+  });
+});
