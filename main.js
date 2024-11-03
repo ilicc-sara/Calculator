6b4ea1f54dff545a030cb5606d38f5e606977560
Sara <sarailic431@gmail.com>
2024-11-03 18:26:42 +0100

    improving functionalty

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ let secondNumber;
 let operation;
 let result;
 
+let number;
+
 // const add = function (num1, num2) {
 //   result = num1 + num2;
 //   return result;
@@ -32,71 +34,78 @@ const secondNumberEl = document.querySelector(".second");
 const numBtns = document.querySelectorAll(".num-btn");
 const opBtns = document.querySelectorAll(".op-btn");
 
-const getResult = document.querySelector(".equals");
+const equals = document.querySelector(".equals");
 
 const clearBtn = document.querySelector(".clear");
 const deleteBtn = document.querySelector(".delete");
 
 numBtns.forEach(function (num, i) {
   num.addEventListener("click", function (e) {
-    // dodati posebnu klasu na = , maknuti ovaj if
-    if (e.target.dataset.num === "=") return;
-    // firstNumber.textContent = e.target.dataset.num;
-    firstNumber = firstNumberEl.textContent + e.target.dataset.num;
-    firstNumberEl.textContent = firstNumber;
+    number = e.target.dataset.num;
+    firstNumberEl.textContent = firstNumberEl.textContent + number;
+    if (!operation) {
+      firstNumber = Number(firstNumberEl.textContent);
+    } else {
+      secondNumber = Number(firstNumberEl.textContent);
+    }
   });
 });
 
 opBtns.forEach(function (op, i) {
   op.addEventListener("click", function (e) {
     operation = e.target.dataset.op;
-    secondNumber = firstNumber + operation;
-    secondNumberEl.textContent = secondNumber;
-    firstNumberEl.textContent = "";
+    // console.log(operation);
+
+    // firstNumberEl.textContent = number;
+    // secondNumber = secondNumberEl.textContent;
+    secondNumberEl.textContent = firstNumberEl.textContent + operation;
+    // number = secondNumber;
+    firstNumberEl.textContent = " ";
+    // firstNumberEl.textContent = firstNumberEl.textContent + number;
+
+    if (secondNumber && firstNumber && operation === "+") {
+      secondNumberEl.textContent = firstNumber + secondNumber;
+    }
+
+    if (secondNumber && firstNumber && operation === "-") {
+      secondNumberEl.textContent = firstNumber - secondNumber;
+    }
+
+    if (secondNumber && firstNumber && operation === "*") {
+      secondNumberEl.textContent = firstNumber * secondNumber;
+    }
+
+    if (secondNumber && firstNumber && operation === "/") {
+      secondNumberEl.textContent = firstNumber / secondNumber;
+    }
 
     console.log("firstNumber: ", firstNumber);
     console.log("secondNumber:", secondNumber);
     console.log("operation:", operation);
+
+    equals.addEventListener("click", function () {
+      console.log("operation equals:", operation);
+      if (secondNumber && firstNumber && operation === "+") {
+        firstNumberEl.textContent = firstNumber + secondNumber;
+        secondNumberEl.textContent = " ";
+      }
+
+      if (secondNumber && firstNumber && operation === "-") {
+        firstNumberEl.textContent = firstNumber - secondNumber;
+        secondNumberEl.textContent = " ";
+      }
+
+      if (secondNumber && firstNumber && operation === "*") {
+        firstNumberEl.textContent = firstNumber * secondNumber;
+        secondNumberEl.textContent = " ";
+      }
+
+      if (secondNumber && firstNumber && operation === "/") {
+        firstNumberEl.textContent = firstNumber / secondNumber;
+        secondNumberEl.textContent = " ";
+      }
+    });
   });
-});
-
-getResult.addEventListener("click", function () {
-  console.log("operation in getResult:", operation);
-  if (secondNumber.includes("+")) {
-    result =
-      Number(secondNumberEl.textContent.replace("+", " ")) +
-      Number(firstNumberEl.textContent);
-    console.log(result);
-    firstNumberEl.textContent = result;
-    secondNumberEl.textContent = " ";
-  }
-
-  if (secondNumber.includes("-")) {
-    result =
-      Number(secondNumberEl.textContent.replace("-", " ")) -
-      Number(firstNumberEl.textContent);
-    console.log(result);
-    firstNumberEl.textContent = result;
-    secondNumberEl.textContent = " ";
-  }
-
-  if (secondNumber.includes("*")) {
-    result =
-      Number(secondNumberEl.textContent.replace("*", " ")) *
-      Number(firstNumberEl.textContent);
-    console.log(result);
-    firstNumberEl.textContent = result;
-    secondNumberEl.textContent = " ";
-  }
-
-  if (secondNumber.includes("/")) {
-    result =
-      Number(secondNumberEl.textContent.replace("/", " ")) /
-      Number(firstNumberEl.textContent);
-    console.log(result);
-    firstNumberEl.textContent = result;
-    secondNumberEl.textContent = " ";
-  }
 });
 
 clearBtn.addEventListener("click", function () {
